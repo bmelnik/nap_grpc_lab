baf33659f8a748a8cff22824f1812bb070641757
Boris Melnik <bmelnik0@gmail.com>
2021-01-24 17:36:51 +0200

    1st commit

---
+++ grpcurl/grpcurl.sh
@@ -0,0 +1,9 @@
+wget https://github.com/fullstorydev/grpcurl/releases/download/v1.7.0/grpcurl_1.7.0_linux_x86_64.tar.gz
+tar -xvf grpcurl_1.7.0_linux_x86_64.tar.gz
+chmod +x grpcurl
+
+#cleanup
+rm grpcurl_1.7.0_linux_x86_64.tar.gz
+rm LICENSE
+
+
